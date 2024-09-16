133e038e5029fd1ecd1c1e1a695f2ed3709419d0
Vijayanand Premnath <vpvijayanand@gmail.com>
2024-09-16 10:42:50 +0530

    Create main.tf
    main.tf created

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tfgcpdemo-99383"
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name     = "storage-tfgcpdemo-99383"
+  location = "US"
+}
